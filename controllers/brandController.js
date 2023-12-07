1d3a3c273ea5b28ad5f255dd8fad9f09d2ca64df
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-12-07 11:14:09 -0700

    Add brand read views

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -3,7 +3,11 @@ const Product = require('../models/product');
 const asyncHandler = require('express-async-handler');
 
 exports.brand_list = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented: Brand list");
+  const allBrands = await Brand.find().exec();
+  res.render('brand_list', {
+    title: 'All Brands',
+    all_brands: allBrands,
+  });
 });
 
 exports.brand_detail = asyncHandler(async (req, res, next) => {
